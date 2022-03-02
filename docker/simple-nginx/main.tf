126c97362d90bec98c230de81cea15adac2d0ffc
Morteza <44939554+Mozart4242@users.noreply.github.com>
2022-03-03 02:28:50 +0330

    Create main.tf

---
+++ docker/simple-nginx/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "tcp://username@serever_ip:2375"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "test"
+  ports {
+    internal = 80
+    external = 80
+  }
+}

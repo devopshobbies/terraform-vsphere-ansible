4a2b35539d21d14aae40cde14e68dd4b8ebb38b1
mozart4242 <sir.morteza4242@gmail.com>
2022-03-22 21:37:57 +0430

    Added Jenkins to repo

---
+++ Jenkins/main.tf
@@ -0,0 +1,132 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.1.1"
+    }
+  }
+}
+
+#===============================================================================
+# vSphere Provider
+#===============================================================================
+
+provider "vsphere" {
+  vsphere_server       = var.vsphere_vcenter
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  allow_unverified_ssl = var.vsphere_unverified_ssl
+}
+
+#===============================================================================
+# vSphere Data
+#===============================================================================
+
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vm_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vm_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vm_template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+#===============================================================================
+# vSphere Resources
+#===============================================================================
+
+resource "vsphere_virtual_machine" "standalone" {
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.vm_cpu
+  memory   = var.vm_ram
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "${var.vm_name}.vmdk"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    linked_clone  = var.vm_linked_clone
+
+    customize {
+      timeout = "20"
+
+      linux_options {
+        host_name = var.vm_name
+        domain    = var.vm_domain
+      }
+
+      network_interface {
+        ipv4_address = var.vm_ip
+        ipv4_netmask = var.vm_netmask
+      }
+
+      ipv4_gateway    = var.vm_gateway
+      dns_server_list = ["${var.vm_dns}"]
+    }
+  }
+}
+
+# In this section we are going to deal with Ansible in the VM we created before.
+resource "local_file" "vm_ip" {
+    content  = vsphere_virtual_machine.standalone.default_ip_address
+    filename = "vm_ip.txt"
+}
+
+resource "null_resource" "nullremote1" {
+depends_on = [vsphere_virtual_machine.standalone] 
+connection {
+ type     = "ssh"
+ user     = "root"
+ password = "${var.password}"
+ host     = "${var.host}" 
+}
+provisioner "file" {
+    source      = "vm_ip.txt"
+    destination = "/root/vm_ip.txt"
+       }
+provisioner "file" {
+    source      = "ansible.yml"
+    destination = "/root/ansible.yml"
+       }
+}
+
+# Run command on the remote vm
+provisioner "remote-exec" {
+ inline = [
+ "apt update",
+ "apt install ansible -y",
+ "cd /root",
+ "ansible-playbook jenkins.yml"
+]
+}
+output "my_ip_address" {
+  value = vsphere_virtual_machine.standalone.default_ip_address
+}
